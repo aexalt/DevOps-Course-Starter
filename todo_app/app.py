d2048706e7158678b49c1e59946fb0c38930a659
aexalt <xandyt@gmail.com>
2022-01-19 15:42:10 +0000

    add

--- todo_app/app.py
+++ todo_app/app.py
@@ -3,6 +3,8 @@ from flask import request
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items
 from todo_app.data.session_items import add_item
+from todo_app.data.session_items import get_item
+from todo_app.data.session_items import save_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -24,3 +26,14 @@ def add():
     # the code below is executed if the request method
     # was GET or the credentials were invalid
     return render_template('index.html', success="error ", result = get_items())
+
+@app.route('/complete', methods=['GET'])
+def complete():
+    error = None
+    if not request.args.get('id','').isspace():
+        item = get_item(request.args.get('id',''))
+        item.status = 'complete'
+        save_item(item)
+        return render_template('index.html', success="successfully written", result = get_items())
+    else:
+        return render_template('index.html', success="didnt get an item id", result = get_items())
