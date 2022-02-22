7302f21edebf19a7c72f6047c48ee9c7ade47b9b
aexalt <xandyt@gmail.com>
2022-02-22 15:03:09 +0000

    c1

--- todo_app/app.py
+++ todo_app/app.py
@@ -8,6 +8,7 @@ from todo_app.data.session_items import save_item
 from todo_app.data.trello_items import trello_get_items
 from todo_app.data.trello_items import trello_add_item
 from todo_app.data.trello_items import trello_complete_item
+from todo_app.data.trello_items import trello_todo_item
 
 import requests
 import os
@@ -24,7 +25,7 @@ def index():
 @app.route('/add', methods=['POST'])
 def add():
     if not request.form['title'].isspace():
-        add_result = trello_add_item(request.form['title'])
+        add_result = trello_add_item(request.form['title'], request.form['desc'])
         return render_template('index.html', success=add_result, result = trello_get_items())
     else:
         return render_template('index.html', success="No title given", result = trello_get_items())
@@ -35,5 +36,14 @@ def complete():
     if not request.args.get('id','').isspace():
         complete_result = trello_complete_item(request.args.get('id',''))
         return render_template('index.html', success=complete_result, result = trello_get_items())
+    else:
+        return render_template('index.html', success="didnt get an item id", result = get_items())
+
+@app.route('/reset', methods=['GET'])
+def reset():
+    error = None
+    if not request.args.get('id','').isspace():
+        reset_result = trello_todo_item(request.args.get('id',''))
+        return render_template('index.html', success=reset_result, result = trello_get_items())
     else:
         return render_template('index.html', success="didnt get an item id", result = get_items())
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -17,16 +17,16 @@ def trello_get_items():
             session["done_list_id"] = trello_list['id']
         for card in trello_list['cards']:
             Items.append(Item.from_trello_card(card, trello_list))
-    print(Items)
     return Items
 
-def trello_add_item(title):
+def trello_add_item(title, desc):
 
     uri_path = "/1/cards"
 
     querys = {
         "idList": session.get("todo_list_id"),
-        "name": title
+        "name": title,
+        "desc": desc
     }
 
     response = call_trello_api(uri_path, "POST", querys)
@@ -38,12 +38,25 @@ def trello_complete_item(id):
     uri_path = "/1/cards/" + id
 
     querys = {
-        "idList": session.get("done_list_id"),
+        "idList": session.get("done_list_id")
     }
 
     response = call_trello_api(uri_path, "PUT", querys)
 
     return http_status_text(response)
+
+def trello_todo_item(id):
+
+    uri_path = "/1/cards/" + id
+
+    querys = {
+        "idList": session.get("todo_list_id"),
+    }
+
+    response = call_trello_api(uri_path, "PUT", querys)
+
+    return http_status_text(response)
+
 
 def call_trello_api(uri_path,httpMethod,add_querys):
 
@@ -63,12 +76,13 @@ def  http_status_text(response):
         return "failed"
 
 class Item: 
-    def __init__(self, id, name, status = 'To Do'): 
+    def __init__(self, id, name, desc, status = 'To Do'): 
         self.id = id 
         self.name = name 
+        self.desc = desc 
         self.status = status 
     
     @classmethod 
     def from_trello_card(cls, card, list): 
-        return cls(card['id'], card['name'], list['name']) 
+        return cls(card['id'], card['name'], card['desc'], list['name']) 
 
