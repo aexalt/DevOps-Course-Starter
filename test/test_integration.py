b3df2b39be9ec649ae5dbeb41492f35d4f3026d1
aexalt <xandyt@gmail.com>
2022-10-31 10:55:29 +0000

    upd

--- test/test_integration.py
+++ test/test_integration.py
@@ -4,19 +4,32 @@ from dotenv import load_dotenv, find_dotenv
 from todo_app import app
 import mongomock
 
+
 @pytest.fixture
 def client():
- file_path = find_dotenv('.env.test')
- load_dotenv(file_path, override=True)
+    file_path = find_dotenv('.env.test')
+    load_dotenv(file_path, override=True)
  
- with mongomock.patch(servers=(('fakemongo.com', 27017),)):
-    test_app = app.create_app()
-    with test_app.test.test_client() as client:
-        yield client
+    with mongomock.patch(servers=(('fakemongo.com', 27017),)):
+        test_app = app.create_app()
+        with test_app.test_client() as client:
+            yield client
 
 def test_index_page(monkeypatch, client):
-    monkeypatch.setattr(trello_items, 'call_trello_api', get_lists_stub)
-    response = client.pymongo.MongoClient()
+    monkeypatch.setattr(trello_items, 'call_trello_api', [{
+            "_id": "234234",
+            "name": "titletest",
+            "desc": "desc",
+            "due": "01/01/2022",
+            "status": "To Do"
+        },{
+            "_id": "234235",
+            "name": "titletest1",
+            "desc": "desc1",
+            "due": "01/01/2023",
+            "status": "To Do"
+        }])
+    response = client.get('/')
     assert response.acknowledged
     assert 'Test card' in response.data.decode()
 
@@ -27,6 +40,23 @@ def test_complete(monkeypatch, client):
     assert response.status_code == 200
     assert 'successful' in response.data.decode()
 
+def test_mongoindex(monkeypatch, client):
+    def mock_db(*args):
+        mock = mongomock.MongoClient().get_database("Fakemongo")
+        mock.todo.insert_one({
+            "_id": "234234",
+            "name": "titletest",
+            "desc": "desc",
+            "due": "01/01/2022",
+            "status": "To Do"
+        })
+        return mock
+
+    monkeypatch.setattr(trello_items, "get_db", mock_db)
+
+    response = client.get('/')
+    assert "titletest" in str(response.data)
+
 class StubResponse():
     def __init__(self, fake_response_data):
         self.fake_response_data = fake_response_data
@@ -36,8 +66,10 @@ class StubResponse():
 
 def get_lists_stub(uri_path,httpMethod,add_querys):
     fake_response_data = [{
-            'id': '123abc',
-            'name': 'To Do',
-            'cards': [{'id': '456', 'name': 'Test card', 'desc': 'test desc', 'due' : None}],
-     }]
+            "id": "234234",
+            "name": "titletest",
+            "desc": "desc",
+            "due": "01/01/2022",
+            "status": "To Do"
+        }]
     return StubResponse(fake_response_data)
