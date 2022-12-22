7977404f5cd2e6cd65634ee9f4be2c159d1779a1
aexalt <xandyt@gmail.com>
2022-12-22 12:52:16 +0000

    initial p11

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
-from flask import request
+from flask import request, redirect
+from flask_login import LoginManager
+from flask_login import login_required
 from todo_app.flask_config import Config
 from todo_app.data.trello_items import trello_get_items
 from todo_app.data.trello_items import trello_add_item
@@ -13,13 +15,41 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
 
+    login_manager = LoginManager()
+    @login_manager.unauthorized_handler
+    def unauthenticated():
+        url = f"https://github.com/login/oauth/authorize?client_id={os.getenv('CLIENT_ID')}"
+        return redirect(url)
+        pass # Add logic to redirect to the GitHub OAuth flow when unauthenticated
+    @login_manager.user_loader
+    def load_user(user_id):
+        pass # We will return to this later
+    login_manager.init_app(app)
+
+    @app.route('/login/callback')
+    def callback():
+        params = {
+            "code": request.args['code'],
+            "client_id": os.getenv('CLIENT_ID'),
+            "client_secret": os.getenv('CLIENT_SECRET')
+        }
+        headers = {"Accept": "application/json"}
+        response = requests.post("https://github.com/login/oauth/access_token", params = params, headers = headers)
+
+        auth_header= {"Authorization": f"Bearer {response.json()['access_token']}"}
+
+        api_response = requests.get("https://api.github.com/user", headers = auth_header)
+
 
     @app.route('/')
+    @login_required
     def index():
         item_view_model = ViewModel(items = trello_get_items(), result_message=None)
         return render_template('index.html', view_model=item_view_model)
 
+    
     @app.route('/add', methods=['POST'])
+    @login_required
     def add():
         if not request.form['title'].isspace():
             result_message = trello_add_item(request.form['title'], request.form['desc'], request.form['datepicker'])
@@ -29,6 +59,7 @@ def create_app():
         return render_template('index.html', view_model=item_view_model)
 
     @app.route('/complete', methods=['GET'])
+    @login_required
     def complete():
         if request.args.get('id'):
             result_message = trello_complete_item(request.args.get('id',''))
@@ -38,6 +69,7 @@ def create_app():
         return render_template('index.html', view_model=item_view_model)
 
     @app.route('/reset', methods=['GET'])
+    @login_required
     def reset():
         if request.args.get('id'):
             result_message = trello_todo_item(request.args.get('id',''))
