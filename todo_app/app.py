1618995e0bb1e85303e68a6b899ea1816cd9f980
aexalt <xandyt@gmail.com>
2022-12-22 13:10:18 +0000

    fix test

--- todo_app/app.py
+++ todo_app/app.py
@@ -14,6 +14,7 @@ import os
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
+    app.config['LOGIN_DISABLED'] = os.getenv('LOGIN_DISABLED') == 'True'
 
     login_manager = LoginManager()
     @login_manager.unauthorized_handler
